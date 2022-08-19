eff23255f0d114734f0383680f989a564905bf57
martingutierrez23 <yeyesill2303@gmail.com>
2022-08-18 22:47:12 -0300

    evento

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
+const carrito=[];
+
 function saludar(){
     alert("bienvenidos a nuestra pagina web")
 }
-function cuaderno (color, marca, precio) {
+saludar();
+//objeto
+function cuaderno (color,marca,precio) {
     this.color=color;
     this.marca=marca;
     this.precio=precio;
 }
-const cuadernoNaranja= new cuaderno("naranja, exito, $700");
-const cuadernoRojo= new cuaderno("rojo, exito, $720");
-const cuadernoAzul= new cuaderno("azul, exito, $710");
-console.log(cuadernoAzul);
+const cuadernoNaranja= new cuaderno("naranja","exito","$700");
+const cuadernoRojo= new cuaderno("rojo","exito","$720");
+const cuadernoAzul= new cuaderno("azul","exito","$710");
 
-saludar();
 
-let cantidad = parseInt(prompt( "Cantidad de productos que deseas comprar"))
-let precio = 4500;
-let total = cantidad * precio;
-let precioConDescuento = total * 0.95   
-alert ("Su compra es de " + total + " pesos");
-
-if (total >= 9000){
-    alert("Tienes un descuento del 5%")
-    alert("tu precio con descuento es de $ "+precioConDescuento )
-    alert("gracias por su compra")
+//creando evento
+let cuaderno1=document.getElementById("cuadernoRojo");
+let cuaderno2=document.getElementById("cuadernoNaranja");
+let cuaderno3=document.getElementById("cuadernoAzul");
+//evento de carrito
+cuaderno1.onclick=()=>{
+    alert("Agregaste un cuaderno rojo al carrito");
+    carrito.push(cuadernoRojo);
+    console.log(carrito);
 }
-
-else if (total < 9000){
-    alert("gracias por su compra")
+cuaderno2.onclick=()=>{
+    alert("Agregaste un cuaderno naranja al carrito");
+    carrito.push(cuadernoNaranja);
+    console.log(carrito);
+}
+cuaderno3.onclick=()=>{
+    alert("Agregaste un cuaderno azul al carrito");
+    carrito.push(cuadernoAzul);
+    console.log(carrito);
 }
